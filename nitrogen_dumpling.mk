db8b552efef36ac1a3d4b2b5ec05bc615136e095
nazmulhasan12 <nazmulhasanicaew@gmail.com>
2018-04-02 11:59:16 +0600

    Update and rename omni_oneplus5.mk to nitrogen_dumpling.mk

--- nitrogen_dumpling.mk
+++ nitrogen_dumpling.mk
@@ -33,31 +33,34 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
 TARGET_BOOTANIMATION_SIZE := 1080p
 
 # Inherit from our custom product configuration
-$(call inherit-product, vendor/omni/config/common.mk)
+$(call inherit-product, vendor/nitrogen/products/common.mk)
 
 # Inherit from hardware-specific part of the product configuration
-$(call inherit-product, device/oneplus/oneplus5/device.mk)
+$(call inherit-product, device/oneplus/dumpling/device.mk)
 
 PRODUCT_PROPERTY_OVERRIDES += ro.hardware.nfc_nci=nqx.default
 
 ALLOW_MISSING_DEPENDENCIES := true
 
-DEVICE_PACKAGE_OVERLAYS += device/oneplus/oneplus5/overlay/device
+DEVICE_PACKAGE_OVERLAYS += device/oneplus/dumpling/overlay/device
 
 PRODUCT_PACKAGES += \
     android.hardware.biometrics.fingerprint@2.1-service
 
 # Discard inherited values and use our own instead.
-PRODUCT_NAME := omni_oneplus5
-PRODUCT_DEVICE := oneplus5
+PRODUCT_NAME := nitrogen_dumpling
+PRODUCT_DEVICE := dumpling
 PRODUCT_BRAND := OnePlus
 PRODUCT_MANUFACTURER := OnePlus
-PRODUCT_MODEL := ONEPLUS A5000
+PRODUCT_MODEL := ONEPLUS A5010
 
-PRODUCT_BUILD_PROP_OVERRIDES += TARGET_DEVICE=OnePlus5 PRODUCT_NAME=OnePlus5
+TARGET_VENDOR_PRODUCT_NAME := OnePlus5T
+TARGET_VENDOR_DEVICE_NAME := OnePlus5T
+
+PRODUCT_BUILD_PROP_OVERRIDES += TARGET_DEVICE=OnePlus5T PRODUCT_NAME=OnePlus5T
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    BUILD_FINGERPRINT=OnePlus/OnePlus5/OnePlus5:8.0.0/OPR6.170623.013/12140945:user/release-keys \
-    PRIVATE_BUILD_DESC="OnePlus5-user 8.0.0 OPR6.170623.013 57 release-keys"
+    BUILD_FINGERPRINT=OnePlus/OnePlus5T/OnePlus5T:8.0.0/OPR6.170623.013/01132343:user/release-keys \
+    PRIVATE_BUILD_DESC="OnePlus5T-user 8.0.0 OPR6.170623.013 263 release-keys"
 
 TARGET_VENDOR := oneplus
