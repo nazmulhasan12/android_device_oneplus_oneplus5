ecb0d3082b4789409ca0a078c172d562c7406f2e
nazmulhasan12 <nazmulhasanicaew@gmail.com>
2018-04-02 11:43:44 +0600

    Update Android.mk

--- DeviceParts/Android.mk
+++ DeviceParts/Android.mk
@@ -1,5 +1,5 @@
 LOCAL_PATH := $(call my-dir)
-ifeq ($(TARGET_DEVICE),$(filter $(TARGET_DEVICE),oneplus5 oneplus5t))
+ifeq ($(TARGET_DEVICE),$(filter $(TARGET_DEVICE),oneplus5 dumpling oneplus5t))
 include $(CLEAR_VARS)
 
 LOCAL_MODULE_TAGS := optional
